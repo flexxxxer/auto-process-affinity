c218813b5b076cb36a9a6c2edf2260afe7bd4c2e
flexxxxer <bahx4pc@gmail.com>
2023-09-26 20:35:44 +0300

    wrapper for runnable processes service added

---
+++ UI/DomainWrappers/CurrentlyRunnableProcessesServiceWrapper.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+
+using System;
+
+using Microsoft.Extensions.Options;
+
+namespace UI.DomainWrappers;
+
+class CurrentlyRunnableProcessesServiceWrapper : IDisposable
+{
+  readonly CurrentlyRunnableProcessesService _processesService;
+
+  public CurrentlyRunnableProcessesServiceWrapper(CurrentlyRunnableProcessesService processesService, 
+    IOptionsMonitor<AppSettings> options)
+  {
+    _processesService = processesService;
+    HandleSettingsChanged(options.CurrentValue);
+    options.OnChange(HandleSettingsChanged);
+  }
+
+  void HandleSettingsChanged(AppSettings newSettings)
+  {
+    _processesService.UpdateInterval = newSettings.RunningProcessesUpdatePeriod;
+    newSettings
+      .ConfiguredProcesses
+      .ForEach(p => _processesService.ProcessNamesToExclude.TryAdd(p.Name, default));
+  }
+
+  public void Dispose() => _processesService.Dispose();
+}
