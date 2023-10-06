b17ee8594cda1fe9deb85662277e10a33fc8d500
flexxxxer <bahx4pc@gmail.com>
2023-10-06 21:31:45 +0300

    AppSettingChangeService not observing changes from filesystem fix

--- UI/DomainWrappers/AppSettingChangeService.cs
+++ UI/DomainWrappers/AppSettingChangeService.cs
@@ -10,6 +10,8 @@ using System.Text.Json;
 
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using System.Reactive.Disposables;
+using ReactiveUI;
 
 namespace UI.DomainWrappers;
 
@@ -19,10 +21,28 @@ public sealed class AppSettingChangeService
   readonly SemaphoreSlim _lockGuard = new(1);
   AppSettings _currentAppSettings;
 
-  public AppSettingChangeService(IHostEnvironment hostEnvironment, IOptions<AppSettings> appSettings)
+  public AppSettingChangeService(IHostEnvironment hostEnvironment, IOptionsMonitor<AppSettings> appSettings)
   {
     _hostEnvironment = hostEnvironment;
-    _currentAppSettings = appSettings.Value;
+    _currentAppSettings = appSettings.CurrentValue;
+
+    appSettings
+      .OnChange(((Action<AppSettings>)HandleAppSettingsChanged)
+        .ThrottleInvokes(TimeSpan.FromSeconds(1)))
+      ?.DisposeWith(App.AppLifetimeDisposable);
+  }
+
+  void HandleAppSettingsChanged(AppSettings newAppSettings)
+  {
+    _lockGuard.Wait();
+    try
+    {
+      _currentAppSettings = newAppSettings;
+    }
+    finally
+    {
+      _lockGuard.Release();
+    }
   }
 
   public async Task MakeChangeAsync(Func<AppSettings, AppSettings> makeChangeFunc)
