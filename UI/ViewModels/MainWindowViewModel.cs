546bf20e533658c53d8e11c1683afd62edcf56a7
flexxxxer <bahx4pc@gmail.com>
2023-10-06 20:06:20 +0300

    position of the program window at startup in accordance with the settings implemented

--- UI/ViewModels/MainWindowViewModel.cs
+++ UI/ViewModels/MainWindowViewModel.cs
@@ -1,40 +1,87 @@
-﻿using Domain;
+﻿using UI.DomainWrappers;
 
+using Domain;
+using Domain.Infrastructure;
+
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Diagnostics;
-using System.Linq;
 
 using Avalonia;
+using Avalonia.Controls;
 
 using ReactiveUI;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 
 using Hardware.Info;
-using UI.DomainWrappers;
-using Domain.Infrastructure;
+
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace UI.ViewModels;
 
 public interface IMainWindowViewModel : IScreen
 {
-  double WindowHeight { get; }
+  double WindowHeight { get; set; }
 
-  double WindowWidth { get; }
+  double WindowWidth { get; set; }
 
   string WindowTitleText { get; }
+
+  WindowStartupLocation WindowStartupLocationMode { get; }
+
+  Interaction<PixelPoint, Unit> SetWindowPosition { get; }
 }
 
 public partial class MainWindowViewModel : ViewModelBase, IMainWindowViewModel, IActivatableViewModel
 {
-  [ObservableProperty] double _windowHeight = 10;
-  [ObservableProperty] double _windowWidth = 10;
+  [ObservableProperty] double _windowHeight;
+  [ObservableProperty] double _windowWidth;
   [ObservableProperty] string _windowTitleText = "";
+  [ObservableProperty] WindowStartupLocation _windowStartupLocationMode;
 
   public string DefaultWindowTitleText { get; }
 
   public ViewModelActivator Activator { get; } = new();
 
-  public MainWindowViewModel(HardwareInfo hwInfo, AdminPrivilegesStatus privilegesStatus) 
+  public Interaction<PixelPoint, Unit> SetWindowPosition { get; } = new();
+
+  public MainWindowViewModel(HardwareInfo hwInfo, AdminPrivilegesStatus privilegesStatus, IOptionsMonitor<AppSettings> appSettings) 
+  {
+    var startupOptions = appSettings.CurrentValue.StartupOptions;
+    WindowTitleText = DefaultWindowTitleText = MakeDefaultTitlePostfix(privilegesStatus);
+    (WindowHeight, WindowWidth) = startupOptions.StartupSizeMode switch
+    {
+      StartupSizeMode.Optimal => MakeOptimalStartupSize(hwInfo),
+      StartupSizeMode.Specified or StartupSizeMode.RememberLast or _ => TupleFrom(startupOptions.StartupSize)
+    };
+    (var locationValues, WindowStartupLocationMode) = startupOptions.StartupLocationMode switch
+    {
+      StartupLocationMode.CenterScreen => (null as (int, int)?, WindowStartupLocation.CenterScreen),
+      StartupLocationMode.RememberLast => (TupleFrom(startupOptions.StartupLocation), WindowStartupLocation.Manual),
+      StartupLocationMode.Default or _ => (null, WindowStartupLocation.Manual),
+    };
+    this.WhenActivated(async (CompositeDisposable d) =>
+    {
+      if(locationValues is (var x, var y))
+      {
+        await Task.Yield(); // "wait" for full view activation (and interaction registrations)
+        await SetWindowPosition.Handle(new(x, y));
+      }
+    });
+  }
+
+  static (int, int) TupleFrom(StartupOptions.StartupLocationValues locationValues)
+    => (locationValues.X, locationValues.Y);
+
+  static (double, double) TupleFrom(StartupOptions.StartupSizeValues sizeValues)
+    => (sizeValues.Height, sizeValues.Width);
+
+  static string MakeDefaultTitlePostfix(AdminPrivilegesStatus privilegesStatus)
   {
     var defaultTitlePostfix = OSTypeApi.CurrentOS switch
     {
@@ -43,20 +90,22 @@ public partial class MainWindowViewModel : ViewModelBase, IMainWindowViewModel, 
       _ => ""
     };
 
-    DefaultWindowTitleText = Process.GetCurrentProcess()
-      .ProcessName.Remove(".exe")
+    return Process.GetCurrentProcess()
+      .ProcessName.Remove(".exe") 
       + defaultTitlePostfix;
+  }
 
-    WindowTitleText = DefaultWindowTitleText;
-
+  static (double Height, double Width) MakeOptimalStartupSize(HardwareInfo hwInfo)
+  {
     hwInfo.RefreshVideoControllerList();
     var verticalRes = hwInfo
         .VideoControllerList
         .MaxBy(vc => vc.CurrentVerticalResolution)
         ?.CurrentVerticalResolution ?? 1080;
 
-    WindowHeight = verticalRes / 1.8;
-    WindowWidth = WindowHeight / 1.5;
+    var height = verticalRes / 1.8;
+    var width = height / 1.5;
+    return (height, width);
   }
 
   // fake one router: MainWindowViewModel not used as IScreen but implements it cause
@@ -66,11 +115,15 @@ public partial class MainWindowViewModel : ViewModelBase, IMainWindowViewModel, 
 
 public sealed class DesignMainWindowViewModel : ViewModelBase, IMainWindowViewModel
 {
-  public double WindowHeight { get; } = 400;
-
-  public double WindowWidth { get; } = 266;
+  public double WindowHeight { get; set; } = 400;
+  
+  public double WindowWidth { get; set; } = 266;
 
   public string WindowTitleText { get; } = Application.Current?.Name ?? "";
+
+  public WindowStartupLocation WindowStartupLocationMode { get; } = WindowStartupLocation.Manual;
+
+  public Interaction<PixelPoint, Unit> SetWindowPosition { get; } = new();
 
   // fake one router: MainWindowViewModel not used as IScreen but implements it cause
   // if not then will be exception when MainWindow.DataContext will be set (ReactiveUI side problem)
--- UI/Views/MainWindow.axaml.cs
+++ UI/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 ﻿using UI.ViewModels;
 
 using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using System;
 
 namespace UI.Views;
 
@@ -9,5 +12,22 @@ public partial class MainWindow : ReactiveWindow<IMainWindowViewModel>
   public MainWindow()
   {
     InitializeComponent();
+
+    this.WhenActivated((CompositeDisposable d) =>
+    {
+      this.WhenAnyValue(w => w.ViewModel)
+        .WhereNotNull()
+        .Subscribe(vm =>
+        {
+          vm.SetWindowPosition
+            .RegisterHandler(interaction =>
+            {
+              Position = interaction.Input;
+              interaction.SetOutput(default);
+            })
+            .DisposeWith(d);
+        })
+        .DisposeWith(d);
+    });
   }
 }
