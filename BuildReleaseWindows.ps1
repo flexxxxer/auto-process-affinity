44733b194b28aa48df0437b1df8e27d1ef3e16d7
flexxxxer <bahx4pc@gmail.com>
2023-10-07 21:32:31 +0300

    open folder with build added to build release for windows script

--- BuildReleaseWindows.ps1
+++ BuildReleaseWindows.ps1
@@ -5,6 +5,7 @@ dotnet publish $csprojPath `
   --configuration Release `
   --runtime win-x64 `
   --self-contained true `
-  /p:PublishSingleFile=true `
+  /p:PublishSingleFile=true
 
 Write-Output "Done."
+Invoke-Item -Path (Join-Path $scriptPath "UI.Desktop\bin\Release\net7.0\win-x64\publish")
