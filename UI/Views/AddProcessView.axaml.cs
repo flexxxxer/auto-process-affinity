0bfad54e8446775eebcc90f936aa73950c3a5c76
flexxxxer <bahx4pc@gmail.com>
2023-10-07 20:19:05 +0300

    autofocus textbox on AddProcessView opening

--- UI/Views/AddProcessView.axaml.cs
+++ UI/Views/AddProcessView.axaml.cs
@@ -1,6 +1,7 @@
 using UI.ViewModels;
 
 using Avalonia.ReactiveUI;
+using Avalonia.Interactivity;
 
 namespace UI.Views;
 
@@ -9,5 +10,10 @@ public partial class AddProcessView : ReactiveUserControl<IAddProcessViewModel>
   public AddProcessView()
   {
     InitializeComponent();
+  }
+
+  protected override void OnLoaded(RoutedEventArgs e)
+  {
+    ProcessNameTextBox.Focus();
   }
 }
