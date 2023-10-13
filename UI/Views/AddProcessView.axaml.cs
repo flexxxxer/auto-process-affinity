631302114282ef7f4a6dd20e48358825bf4cb750
flexxxxer <bahx4pc@gmail.com>
2023-10-13 22:28:40 +0300

    view for adding and editing monitored processes ux and ui improvement

--- UI/Views/AddProcessView.axaml.cs
+++ UI/Views/AddProcessView.axaml.cs
@@ -2,6 +2,9 @@ using UI.ViewModels;
 
 using Avalonia.ReactiveUI;
 using Avalonia.Interactivity;
+using Avalonia.Input;
+using System.Linq;
+using ReactiveUI;
 
 namespace UI.Views;
 
@@ -10,6 +13,24 @@ public partial class AddProcessView : ReactiveUserControl<IAddProcessViewModel>
   public AddProcessView()
   {
     InitializeComponent();
+
+    var textBoxesWhereOnlyDigitsAllowed = new[]
+    {
+      EvenAffinityModeFirstNValueTextBox,
+      FirstNAffinityModeValueTextBox,
+      LastNAffinityModeValueTextBox,
+    };
+
+    foreach(var tb in textBoxesWhereOnlyDigitsAllowed)
+    {
+      tb.AddHandler(TextInputEvent, TextBox_TextInput_AllowOnlyDigits, RoutingStrategies.Tunnel);
+    }
+  }
+
+  private void TextBox_TextInput_AllowOnlyDigits(object? _, TextInputEventArgs e)
+  {
+    static bool NotDigit(char c) => char.IsDigit(c);
+    e.Handled = e.Text?.Any(NotDigit) is not true;
   }
 
   protected override void OnLoaded(RoutedEventArgs e)
