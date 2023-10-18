986f2f6c7b1b1fdc9917a51311d6992622411cf5
flexxxxer <bahx4pc@gmail.com>
2023-10-18 18:34:29 +0300

    new converters added

---
+++ UI/Converters/ArithmeticConverters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using Avalonia.Data.Converters;
+
+namespace UI.Converters;
+
+public static class ArithmeticConverters
+{
+  public static readonly IMultiValueConverter FirstGreaterThanSecond
+    = new FuncMultiValueConverter<IComparable, bool>(x =>
+    {
+      var toCompare = x.ToArray();
+
+      return toCompare switch
+      {
+        [] => false,
+        [var first, var second] => first?.CompareTo(second) > 0,
+        [..] => false
+      };
+    });
+  
+  public static readonly IMultiValueConverter FirstLessThanSecond
+    = new FuncMultiValueConverter<IComparable, bool>(x =>
+    {
+      var toCompare = x.ToArray();
+
+      return toCompare switch
+      {
+        [] => false,
+        [var first, var second] => first?.CompareTo(second) < 0,
+        [..] => false
+      };
+    });
+}
---
+++ UI/Converters/BoolToSelectionModeConverters.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+
+namespace UI.Converters;
+
+public static class BoolToSelectionModeConverters
+{
+  /// <summary>
+  /// <see langword="null"/> match <see cref="SelectionMode.Toggle"/>,
+  /// <see langword="false"/> match <see cref="SelectionMode.Single"/>,
+  /// <see langword="true"/> match <see cref="SelectionMode.Multiple"/>
+  /// </summary>
+  public static readonly IValueConverter ToSelectionMode
+    = new FuncValueConverter<bool?, SelectionMode>(val => val switch
+    {
+      null => SelectionMode.Toggle,
+      false => SelectionMode.Single,
+      true => SelectionMode.Multiple
+    });
+  
+  /// <summary>
+  /// <see langword="false"/> match <see cref="DataGridSelectionMode.Single"/>,
+  /// <see langword="true"/> match <see cref="DataGridSelectionMode.Extended"/>
+  /// </summary>
+  public static readonly IValueConverter ToDataGridSelectionMode
+    = new FuncValueConverter<bool, DataGridSelectionMode>(val => val switch
+    {
+      false => DataGridSelectionMode.Single,
+      true => DataGridSelectionMode.Extended
+    });
+}
