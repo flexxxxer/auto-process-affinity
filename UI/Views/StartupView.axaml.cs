2b6641d0a5f3210283edb9b3fbb93a23407e473b
flexxxxer <bahx4pc@gmail.com>
2023-10-22 11:42:49 +0300

    behavior for DoubleTapped replaced by event handler for DataGrid on StartupView: bug fixed when double tap on datagrid column cause selecting rows or entering to edit mode

--- UI/Views/StartupView.axaml.cs
+++ UI/Views/StartupView.axaml.cs
@@ -1,16 +1,20 @@
+using Domain;
+
 using UI.ViewModels;
+using UI.ViewModels.Entities;
 
 using System;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 
+using Avalonia.Input;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
+using Avalonia.VisualTree;
 
 using ReactiveUI;
-using UI.ViewModels.Entities;
 
 namespace UI.Views;
 
@@ -26,6 +30,9 @@ public partial class StartupView : ReactiveUserControl<IStartupViewModel>
         .WhereNotNull()
         .Subscribe(vm =>
         {
+          MonitoredProcessesDataGrid.SelectedItem = null;
+          vm.SelectedProcesses = new(Array.Empty<MonitoredProcess>());
+          
           Observable
             .FromEventPattern<SelectionChangedEventArgs>(
               h => MonitoredProcessesDataGrid.SelectionChanged += h,
@@ -35,6 +42,43 @@ public partial class StartupView : ReactiveUserControl<IStartupViewModel>
               .OfType<MonitoredProcess>()
               .ToList()
               .AsReadOnly())
+            .DisposeWith(d);
+          
+          Observable
+            .FromEventPattern<TappedEventArgs>(
+              h => MonitoredProcessesDataGrid.DoubleTapped += h,
+              h => MonitoredProcessesDataGrid.DoubleTapped -= h)
+            .Subscribe(e =>
+            {
+              if (MonitoredProcessesDataGrid.SelectedItems is [var selectedItem]
+                  && e.EventArgs.Source?.TryCastTo<Control>().FindAncestorOfType<DataGridRow>() is not null)
+              {
+                vm.EditMonitoredProcessCommand.ExecuteAsync(selectedItem).NoAwait();
+              }
+            })
+            .DisposeWith(d);
+
+          
+          var whenSortingHappenedLastTime = TimeSpan.Zero;
+          Observable
+            .FromEventPattern<DataGridColumnEventArgs>(
+              h => MonitoredProcessesDataGrid.Sorting += h,
+              h => MonitoredProcessesDataGrid.Sorting -= h)
+            .Subscribe(_ => whenSortingHappenedLastTime = TimeSpan.FromMilliseconds(Environment.TickCount64))
+            .DisposeWith(d);
+          
+          Observable
+            .FromEventPattern<SelectionChangedEventArgs>(
+              h => MonitoredProcessesDataGrid.SelectionChanged += h,
+              h => MonitoredProcessesDataGrid.SelectionChanged -= h)
+            .Subscribe(_ =>
+            {
+              var now = TimeSpan.FromMilliseconds(Environment.TickCount64);
+              if (now - whenSortingHappenedLastTime < TimeSpan.FromMilliseconds(150))
+              {
+                MonitoredProcessesDataGrid.SelectedItem = null;
+              }
+            })
             .DisposeWith(d);
         })
         .DisposeWith(d);
