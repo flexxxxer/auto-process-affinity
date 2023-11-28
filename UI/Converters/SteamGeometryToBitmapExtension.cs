37819006ac7d2fa10d2bc4a7dca4ba9a9401a1c4
flexxxxer <bahx4pc@gmail.com>
2023-11-28 23:18:44 +0300

    tray icon: new buttons added + icons for tray icon buttons

---
+++ UI/Converters/SteamGeometryToBitmapExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+
+namespace UI.Converters;
+
+public class SteamGeometryToBitmapExtension : MarkupExtension
+{
+  public Geometry Geometry { get; set; } = null!;
+  public IBrush Fill { get; set; } = null!;
+
+  public override object ProvideValue(IServiceProvider serviceProvider)
+  {
+    var visual = new Path
+    {
+      Data = Geometry, Fill = Fill,
+    };
+
+    var bitmap = new RenderTargetBitmap(new PixelSize(26, 26));
+    bitmap.Render(visual);
+    return bitmap;
+  }
+}
